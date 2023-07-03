c3a7d96887e730cc038c4b18e0e4eb1fb34727c8
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-03 17:35:59 +0300

    Added SQL scripts for initialising database

---
+++ SQL Queries/ViewCreate.sql
@@ -0,0 +1,17 @@
+USE CustomerOrderViewer;
+
+CREATE VIEW [dbo].[CustomerOrderDetail] AS
+	SELECT 
+		t1.CustomerOrderID, 
+		t2.CustomerID, 
+		t3.ItemId,
+		t2.FirstName, 
+		t2.LastName,
+		t3.[Description],
+		t3.Price
+	FROM 
+		dbo.CustomerOrder t1 
+	INNER JOIN 
+		dbo.Customer t2 ON t2.CustomerId = t1.CustomerId
+	INNER JOIN
+		dbo.Item t3 ON t3.ItemId = t1.ItemId
