301dff056cf57ef6ccb77dec913c75cad35448af
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-04 11:57:24 +0300

    Added classes for SQL command execution

---
+++ CustomerOrderViewer/Repository/CustomerCommand.cs
@@ -0,0 +1,34 @@
+﻿using CustomerOrderViewer.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderViewer.Repository
+{
+    class CustomerCommand
+    {
+        private string _connectionString;
+
+        public CustomerCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IList<CustomerModel> GetList()
+        {
+            List<CustomerModel> list = new List<CustomerModel>();
+            var sql = "Customer_GetList";
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                list = connection.Query<CustomerModel>(sql).ToList();
+            }
+
+            return list;
+        }
+    }
+}
---
+++ CustomerOrderViewer/Repository/ItemCommand.cs
@@ -0,0 +1,34 @@
+﻿using CustomerOrderViewer.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderViewer.Repository
+{
+    class ItemCommand
+    {
+        private string _connectionString;
+
+        public ItemCommand(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        
+        public IList<ItemModel> GetList() 
+        { 
+            List<ItemModel> list = new List<ItemModel>();
+            var sql = "Item_GetList";
+            
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                list = connection.Query<ItemModel>(sql).ToList();
+            }
+
+            return list;
+        }
+    }
+}
